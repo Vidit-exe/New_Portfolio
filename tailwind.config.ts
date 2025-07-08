6b7f9bca0558f3a66c1b4e0d52e3cc257e4744aa
Vidit <=>
2025-07-08 20:38:49 +0530

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,13 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 const config: Config = withMT({
   darkMode: "class",
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // optional
   ],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 });
 
